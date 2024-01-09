660e6483401e5e1f2d3e51d13d767b87159731a8
Uday <udayrana428@gmail.com>
2024-01-09 13:16:43 +0530

    Added Backend

---
+++ server.js
@@ -0,0 +1,38 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const authRoutes = require('./routes/authRoutes.js');
+const projectRoutes = require('./routes/projectRoutes.js');
+const cors=require('cors')
+const PORT = process.env.PORT || 3001;
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://udayrana428:surendra@cluster0.px7ut.mongodb.net/portfolio', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+// Routes
+app.get("/",(req,res)=>{
+    res.send("welcome to my site")
+  })
+// Define your routes here
+app.use('/auth', authRoutes);
+app.use('/projects', projectRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
